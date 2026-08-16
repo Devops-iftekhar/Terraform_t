ac2b7630541dc571ea5df78d932d8e852e4f9445
iftekhar <mdiftekhar.tech@gmail.com>
2026-08-16 08:38:45 +0530

    add 2nd rg

--- main.tf
+++ main.tf
@@ -14,3 +14,7 @@ resource "azurerm_resource_group" "rg" {
   name     = "Dev_Demo_rg"
   location = "Central india"
 }
+resource "azurerm_resource_group" "rg1" {
+  name ="Dev-Demo1_rg"
+  location = "central india"
+}
